2e9ec396d6d81622ff71f5287dd1c9b80c3937cb
Logan Pippin <logan.pippin32@gmail.com>
2020-12-01 23:32:54 -0600

    Finished everyting forgot to commit more my bad.

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # 10_NoteTaker
+
+## Table of Contents
+
+- [Description](#Description)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [License](#License)
+- [Contribution](#Contribution)
+- [Tests](#Tests)
+- [Questions](#Questions)
+
+## Description
+
+It is a web application that allows you to save notes server side.
+
+## Installation
+
+you need to install express.
+
+## Usage
+
+you would click get started then add a title and a description for the note and then hit the save button.
+
+## License
+
+![MIT](https://img.shields.io/badge/license-MIT-brightgreen)
+
+## Contributing
+
+You don't need to contrubute but if you want to you can email me.
+
+## Tests
+
+none
+
+## Questions
+
+- GitHub Username: [LoganPippin](github.com/LoganPippin)
+- Email: logan.pippin32@gmail.com
---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const { fstat } = require("fs");
+const path = require("path");
+const { json } = require("express");
+
+const app = express();
+const PORT = 3000;
+const note = require("./db/db.json") || [];
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function (_, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", function (_, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", function (_, res) {
+  res.json(note);
+});
+
+app.post("/api/notes", function (req, res) {
+  req.body.length = note.length;
+  note.push(req.body);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(note));
+  res.end();
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+  let id = +req.params.id;
+
+  note = note.filter(function (elem) {
+    return elem.id !== id;
+  });
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(note));
+  res.json(note);
+});
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
